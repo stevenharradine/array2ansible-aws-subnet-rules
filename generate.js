b4d80b2f3c7986ba8bafc89c3b2826345fa6d56f
Steven Harradine <stevenharradine@gmail.com>
2016-08-13 17:04:47 -0400

    condense cidr and ports down before writing security groups to minimize the number of groups created

--- generate.js
+++ generate.js
@@ -1,22 +1,33 @@
 config = require ("./config")
 
 security_group_counter = 0
+cidr_port = []
 
 config.ports.forEach (function (portValue) {
 	config.list.forEach (function (value, index, array) {
-		if (index % config.max_security_group_size == 0) {
-			this_security_group_name = config.security_group_name + " " + ++security_group_counter
-			console.log ("  - name: " + this_security_group_name)
-			console.log ("    ec2_group:")
-			console.log ("      name: " + this_security_group_name)
-			console.log ("      description: " + this_security_group_name)
-			console.log ("      vpc_id: " + config.vpc_id)
-			console.log ("      region: " + config.vpc_region)
-			console.log ("      rules:")
-		}
-		console.log ("        - proto: tcp")
-		console.log ("          from_port: " + portValue)
-		console.log ("          to_port: " + portValue)
-		console.log ("          cidr_ip: " + value + (value.indexOf ("/") < 0 === true ? "/32" : ""))
+		cidr_port.push ({
+			"cidr": value,
+			"port":  portValue
+		})
 	})
+})
+
+cidr_port.forEach (function (value, index, array) {
+	cidr = value.cidr
+	port = value.port
+
+	if (index % config.max_security_group_size == 0) {
+		this_security_group_name = config.security_group_name + " " + ++security_group_counter
+		console.log ("  - name: " + this_security_group_name)
+		console.log ("    ec2_group:")
+		console.log ("      name: " + this_security_group_name)
+		console.log ("      description: " + this_security_group_name)
+		console.log ("      vpc_id: " + config.vpc_id)
+		console.log ("      region: " + config.vpc_region)
+		console.log ("      rules:")
+	}
+	console.log ("        - proto: tcp")
+	console.log ("          from_port: " + port)
+	console.log ("          to_port: " + port)
+	console.log ("          cidr_ip: " + cidr + (cidr.indexOf ("/") < 0 === true ? "/32" : ""))
 })
